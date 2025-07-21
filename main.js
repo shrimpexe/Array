1043ec61d1c596c3616bc5584395e5e36903f772
Joseph <notjosephexe@gmail.com>
2025-07-21 20:09:36 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let name1 = window.prompt ('Enter first name')
+let name2 =window.prompt ('Enter second name')
+let name3 =window.prompt('Enter third name ma3lsh')
+let names = [name1, name2, name3]
+console.log(`Your names are: ${names}`)
+console.log(`Your names number is: ${names.length}`)
+console.log('i dont know how to code pls dont laugh at me :(')
